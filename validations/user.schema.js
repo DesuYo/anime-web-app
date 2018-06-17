4fad1929686bc308df599737592d723b64a0a37b
Alex <zaychenko96y@outlook.com>
2018-06-18 01:55:01 +0300

    Fixed user validation schema

--- validations/user.schema.js
+++ validations/user.schema.js
@@ -1,9 +1,7 @@
 const Joi = require('joi')
 
 module.exports = {
-  addUser: Joi.object({
-    username: Joi.string().required().max(15).alphanum(),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,30}$/)
-  })
+  username: Joi.string().required().max(15).alphanum(),
+  email: Joi.string().required().email(),
+  password: Joi.string().required().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,30}$/)
 }
