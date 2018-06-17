ac9c68fba7a28133f18eecc2fc3248261e128f2e
Alex <zaychenko96y@outlook.com>
2018-06-17 22:12:19 +0300

    Fixed schema init in user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
