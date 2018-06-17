7948ab949cb767e5f56eb4c40df8c8a2e04d6844
Alex <zaychenko96y@outlook.com>
2018-06-18 01:42:26 +0300

    Updated validation logic in user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,24 +1,28 @@
 const User = require('../models/user.model')
+const Joi = require('joi')
+const userValidator = require('../validations/user.schema')
 
 module.exports = {
   async signUp (req, res) {
     try {
-      const { email, username, password } = req.body;
+      const { email, username, password } = req.body
 
       const createdUser = new User({
         email,
         username,
         password
       })
+
+      await Joi.validate(createdUser, userValidator)
       await createdUser.save()
 
       return res.status(201).json({
-        message: 'User was successfully created'
+        message: 'User was saved successfully'
       })
     }
     catch (err) {
       return res.status(500).json({
-        message: 'Internal server error'
+        message: err.message
       })
     }
   }
