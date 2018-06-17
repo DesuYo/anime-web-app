7cdbadbdbac44755161a70942e013bc68b79dc55
Alex <zaychenko96y@outlook.com>
2018-06-17 23:54:02 +0300

    Removed 'required' fields from user model

--- models/user.model.js
+++ models/user.model.js
@@ -4,17 +4,14 @@ const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
     unique: true
   },
   email: {
     type: String,
-    required: true,
     unique: true
   },
   password: {
-    type: String,
-    required: true
+    type: String
   }
 })
 
