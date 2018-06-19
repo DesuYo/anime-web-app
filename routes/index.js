e8fcceb9f465c16fdc28920396618f891d0fe29e
Alex <zaychenko96y@outlook.com>
2018-06-20 02:38:00 +0300

    Temporarily removed auth middleware from router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,4 @@ const authorization = require('../services/auth.service')
 
 module.exports = Router()
   .post('/anime', ctrls.anime.addAnime)
-  .post('/signup', authorization.checkAuth(), validator.validateBody(userValidation), ctrls.user.signUp)
+  .post('/signup', validator.validateBody(userValidation), ctrls.user.signUp)
