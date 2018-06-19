9328ef4ddd7f4db67ec6e6f58aa61b49bad1f98e
Alex <zaychenko96y@outlook.com>
2018-06-20 02:51:18 +0300

    Re-implemented signup method of user controller for postgres

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,34 @@
-const User = require('../models/user.model')
+const db = require('../models/index')
 
 module.exports = {
   async signUp (req, res) {
     try {
       const { email, username, password } = req.body
 
-      const createdUser = new User({
+      const createdUser = db['user'].build({
         email,
         username,
         password
       })
       const user = await createdUser.save()
-      const result = User.findById(user._id).select('-password')
+      const result = db['user'].findById(user._id, {
+        attributes: { exclude: ['password'] }
+      })
       
       return res.status(201).json({
         createdUser: result,
         message: 'User was saved successfully'
       })
+    }
+    catch (err) {
+      return res.status(500).json({
+        message: err.message
+      })
+    }
+  },
+  async logIn (req, res) {
+    try {
+
     }
     catch (err) {
       return res.status(500).json({
