62f40dbb24c3758174842099870a85408538f73c
Alex <zaychenko96y@outlook.com>
2018-06-24 21:17:47 +0300

    Implemented user registration

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,35 +19,7 @@ module.exports = {
   },
   async logIn (req, res) {
     try {
-      const { email, username, password } = req.body
-
-      const { Op } = Sequelize
-      const user = await db['user'].findOne({
-        where: { 
-          [Op.or]: [{ username }, { email }]
-        }
-      })
-
-      const checkedPassword = await user.comparePasswordWith(password)
-      if (!checkedPassword) {
-        return res.status(401).json({
-          message: 'Unauthorized user'
-        })
-      }
-
-      const token = jwt.sign({
-        userId: user._id
-      },
-      process.env.JWT_KEY,
-      {
-        expiresIn: "3h"
-      })
-
-      return res.status(200).json({
-        message: 'Authorization completed',
-        userId: user._id,
-        token
-      })
+      
     }
     catch (err) {
       return res.status(500).json({
--- models/user.model.js
+++ models/user.model.js
@@ -5,11 +5,11 @@ const bcrypt = require('bcrypt')
 module.exports = {
   async init() {
     return db.query(`
-      CREATE TABLE IF NOT EXISTS user(
+      CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         username VARCHAR (15) UNIQUE NOT NULL,
         email VARCHAR (50) UNIQUE NOT NULL,
-        password VARCHAR (30) NOT NULL 
+        password VARCHAR (100) NOT NULL 
       )
     `)
   },
@@ -17,9 +17,10 @@ module.exports = {
   async add (user) {
     const { username, email, password } = user
     const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(hashedPassword.length)
 
     return (await db.query({
-      text: `INSERT INTO user(username, email, password) VALUES($1, $2, $3) 
+      text: `INSERT INTO users(username, email, password) VALUES($1, $2, $3) 
         RETURNING username, email`,
       values: [ username, email, hashedPassword ]
     })).rows[0]
@@ -27,7 +28,7 @@ module.exports = {
 
   async getById (id) {
     return (await db.query({
-      text: 'SELECT * FROM user WHERE id = $1',
+      text: 'SELECT * FROM users WHERE id = $1',
       values: [ id ]
     })).rows[0]
   }
