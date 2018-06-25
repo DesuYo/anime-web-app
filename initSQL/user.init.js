563dd0d47a81e3e6035ab9c0042c05b2ce46b881
Alex <zaychenko96y@outlook.com>
2018-06-25 03:37:38 +0300

    Minor fixes

--- initSQL/user.init.js
+++ initSQL/user.init.js
@@ -6,28 +6,3 @@ module.exports = `
     password VARCHAR (100) NOT NULL 
   )
 `
-  /*async getById (id) {
-    return (await db.query({
-      text: 'SELECT * FROM users WHERE id = $1',
-      values: [ id ]
-    })).rows[0]
-  },
-
-  async getId (user) {
-    const { username, email } = user
-    return (await db.query({
-      text: `SELECT id FROM users WHERE username = $1 OR email = $2`,
-      values: [ username, email ]
-    }))
-  },
-
-  async checkPassword (user) {
-    const { username, email, password } = user
-    const modelPassword = (await db.query({
-      text: `SELECT password FROM users WHERE username = $1 OR email = $2`,
-      values: [ username, email ]
-    })).rows[0].password
-
-    return await bcrypt.compare(modelPassword, password)
-  }
-}*/
