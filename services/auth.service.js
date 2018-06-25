c6273f3e66985247d50d6c91f0e0a8e9bef3c2e0
Alex <zaychenko96y@outlook.com>
2018-06-25 21:52:20 +0300

    Implemented auth middleware

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,18 +1,22 @@
 const jwt = require('jsonwebtoken')
 
+const db = require('../connections.pool')
+
 module.exports = {
   checkAuth (req, res, next) {
     try {
       const token = req.headers.authorization.split(" ")[1]
-      console.log(token)
       const decoded = jwt.verify(token, process.env.JWT_KEY)
-      req.userData = decoded
+      const user = db.query({
+        text: 'SELECT id FROM users WHERE id = $1',
+        values: [ decoded.id ]
+      })
 
       next()
     }
-    catch (error) {
-      return res.status(401).json({
-        message: 'Authorization failed.'
+    catch (err) {
+      return res.status(500).json({
+        message: err.message
       })
     }
   }
