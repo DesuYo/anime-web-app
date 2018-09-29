93c3a05edd5233870ec1a7ae8996a25fd8f0c840
Oleh <opavlov@conceptsoft.io>
2018-09-29 15:14:46 +0300

    fix type

--- initSQL/comments.init.js
+++ initSQL/comments.init.js
@@ -2,7 +2,7 @@ module.exports = `
   CREATE TABLE IF NOT EXIST comments {
     id SERIAL PRIMARY KEY,
     text VARCHAR (300) NOT NULL,
-    posted_at DATE DEFAULT CURRENT_DATE,
+    posted_at TIMESTAMPTZ DEFAULT NOW(),
     reply_to REFERENCES comments (id) ON DELETE CASCADE,
     anime_id REFERENCES anime (id) ON DELETE CASCADE,
     owner_id REFERENCES users (id) ON DELETE CASCADE
