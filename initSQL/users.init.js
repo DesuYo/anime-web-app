6915775dcaecba14ee996ebbdf0ad2a961d4e79a
Oleh <opavlov@conceptsoft.io>
2018-09-30 20:52:53 +0300

    structure

--- initSQL/users.init.js
+++
@@ -1,8 +0,0 @@
-module.exports = `
-  CREATE TABLE IF NOT EXISTS users(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR (15) UNIQUE NOT NULL,
-    email VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (100) NOT NULL 
-  )
-`
